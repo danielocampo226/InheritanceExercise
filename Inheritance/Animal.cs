019475b6176f42046dac19a0ce33b3a1b5de7095
danielocampo226 <sierradanny562@gmail.com>
2023-02-01 01:08:29 -0500

    inheritance

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Animal
+    {
+        public Animal()
+        {
+
+        }
+
+        public bool IsAlive { get; set; }
+        public double Age { get; set; }
+
+        public int LegCount { get; set; }
+        public string LandSeaAir { get; set; }
+    }
+}
